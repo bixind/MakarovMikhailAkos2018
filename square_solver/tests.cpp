e424d517c854508c6564bfaeac473479d1e186f9
bixind <bixind@yandex-team.ru>
2018-09-20 11:49:30 +0300

    initializations

--- square_solver/tests.cpp
+++ square_solver/tests.cpp
@@ -5,14 +5,14 @@ TEST_SUITE(SquarePolynomial) {
     TEST(TwoRoots) {
         {
             double polynom[] = {3, -4, 1};
-            double answer[2];
+            double answer[2] = {0, 0};
             UNIT_ASSERT(solveSquarePolynom(polynom, answer) == 2);
             UNIT_ASSERT(isZero(answer[0] - 1));
             UNIT_ASSERT(isZero(answer[1] - 3));
         }
         {
             double polynom[] = {-1, -1, 12};
-            double answer[2];
+            double answer[2] = {0, 0};
             UNIT_ASSERT(solveSquarePolynom(polynom, answer) == 2);
             UNIT_ASSERT(isZero(answer[0] + 1.0/4));
             UNIT_ASSERT(isZero(answer[1] - 1.0/3));
@@ -21,14 +21,14 @@ TEST_SUITE(SquarePolynomial) {
 
     TEST(OneRoot) {
         double polynom[] = {9, 6, 1};
-        double answer[2];
+        double answer[2] = {0, 0};
         UNIT_ASSERT(solveSquarePolynom(polynom, answer) == 1);
         UNIT_ASSERT(isZero(answer[0] + 3));
     }
 
     TEST(NoRoots) {
         double polynom[] = {10, 6, 1};
-        double answer[2];
+        double answer[2] = {0, 0};
         UNIT_ASSERT(solveSquarePolynom(polynom, answer) == 0);
     }
 }
@@ -58,7 +58,7 @@ TEST_SUITE(LinearPolynomial) {
         RootsCount(*solvers[])(const double*, double*) = {solveSquarePolynom, solveLinearPolynom};
         for (size_t i = 0; i < 2; ++i) {
             double polynom[] = {5, 0, 0};
-            double answer[2];
+            double answer[2] = {0, 0};
             UNIT_ASSERT(solvers[i](polynom, answer) == 0);
         }
     }
@@ -67,7 +67,7 @@ TEST_SUITE(LinearPolynomial) {
         RootsCount(*solvers[])(const double*, double*) = {solveSquarePolynom, solveLinearPolynom};
         for (size_t i = 0; i < 2; ++i) {
             double polynom[] = {0, 0, 0};
-            double answer[2];
+            double answer[2] = {0, 0};
             UNIT_ASSERT(solvers[i](polynom, answer) == -1);
         }
     }
